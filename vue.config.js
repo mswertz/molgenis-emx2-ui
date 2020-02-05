d29bd215100e9417ed7464a85893caba78b728fa
mswertz <m.a.swertz@rug.nl>
2020-02-05 14:31:02 +0100

    fix: publish to npm

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 
 module.exports = {
+  publicPath: "", //to ensure relative paths are used
   devServer: {
     proxy: {
       "^/api": { target: "http://localhost:8080" }
