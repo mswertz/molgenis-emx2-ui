029c3889592d114df39b5f1758f9372f6e605c3b
mswertz <m.a.swertz@rug.nl>
2020-02-06 11:21:44 +0100

    fix: added file containing VERSION

--- .releaserc.js
+++ .releaserc.js
@@ -5,6 +5,9 @@
     ["@semantic-release/git", {
       "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }]
+    }],
+    ["@semantic-release/exec", {
+      "prepareCmd": "echo \"${nextRelease.version}\" > dist/VERSION",
+    }],
   ]
 }
